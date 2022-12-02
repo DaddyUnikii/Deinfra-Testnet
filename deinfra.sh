4233e067caf320ed7f42b3fbdd99154202ffdbf5
Unikii <97149134+DaddyUnikii@users.noreply.github.com>
2022-12-02 21:29:22 +0700

    Create deinfra.sh

---
+++ deinfra.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo "==================================================================="
+echo -e "\e[92m"
+echo  "   ______     ___    ______   ______ __     ___    ";
+echo  "  |       \  /   \  |       \        \  \  /   /   ";
+echo  "  |        |/     \ |        |        |  \/   /    ";
+echo  "  |   D    |   A   \    D    |   D    |   Y  /     ";
+echo  "  |        |   _    \        |        ||    |      ";
+echo  "  |______ /__/   \___\_____ / ______ / |____|      "; 
+echo  "  -----------------------------------------------  ";
+echo  "        -Kopi, Udud, dan Bokep siapin bang-        ";
+echo  "                   -biar relax-                    ";
+echo  "       -GARAP NODE ITU PELAN PELAN GARAPNYA-       ";
+echo  "               -DIBACA DENGAN TELITI-              ";
+echo -e "\e[0m"
+echo "===================================================================" 
+
+echo -e '\e[36mTelegram chat :\e[39m' @unikii
+echo -e '\e[36mWebsite :\e[39m' https://github.com/Leevaleeth
+echo "======================================="
